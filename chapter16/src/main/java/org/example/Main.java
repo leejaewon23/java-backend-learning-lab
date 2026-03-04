d68af886d67460021b7f645b3a531526031f0c0f
leejaewon23 <sosekee1110@naver.com>
2026-03-04 18:44:38 +0900

    주석 삭제

--- chapter16/src/main/java/org/example/Main.java
+++ chapter16/src/main/java/org/example/Main.java
@@ -4,8 +4,6 @@ import ramda.NormalClassRamda;
 import ramda.Ramda;
 import ramda.RamdaUseInterface;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         NormalClassRamda nui = new NormalClassRamda();
