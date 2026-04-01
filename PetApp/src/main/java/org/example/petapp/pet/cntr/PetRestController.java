ea2c007628f23c0d4c7eefad1291d5d3c108f889
leejaewon23 <sosekee1110@naver.com>
2026-04-01 20:33:08 +0900

    update 메소드 생성

--- PetApp/src/main/java/org/example/petapp/pet/cntr/PetRestController.java
+++ PetApp/src/main/java/org/example/petapp/pet/cntr/PetRestController.java
@@ -5,10 +5,7 @@ import org.example.petapp.pet.dto.PetDto;
 import org.example.petapp.pet.svc.PetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/pet")
@@ -22,6 +19,16 @@ public class PetRestController {
             PetDto result = this.petService.insert(petDto);
             return ResponseEntity.ok().body(new PetResponseDto(0, "SUCCESS", result));
         } catch (Exception ex) {
+            return ResponseEntity.status(500).body(new PetResponseDto(-999, "ERROR", null));
+        }
+    }
+
+    @PatchMapping
+    public ResponseEntity<PetResponseDto> updatePet(@RequestBody PetDto petDto) {
+        try {
+            PetDto result = this.petService.update(petDto);
+            return ResponseEntity.ok().body(new PetResponseDto(0, "SUCCESS", result));
+        } catch (Exception e) {
             return ResponseEntity.status(500).body(new PetResponseDto(-999, "ERROR", null));
         }
     }
