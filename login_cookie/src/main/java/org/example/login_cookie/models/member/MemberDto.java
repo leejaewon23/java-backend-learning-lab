72cb02e12a242b17d8ee34fd39786ad9a361f2dc
leejaewon23 <sosekee1110@naver.com>
2026-05-21 18:34:55 +0900

    init: MemberDto, MemberEntity, IMember 구조 생성

--- login_cookie/src/main/java/org/example/login_cookie/models/member/MemberDto.java
+++ login_cookie/src/main/java/org/example/login_cookie/models/member/MemberDto.java
@@ -1,4 +1,23 @@
 package org.example.login_cookie.models.member;
 
-public class MemberDto {
+import lombok.*;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MemberDto implements IMember {
+    private Long id;
+    private String signId;
+    private String password;
+    private String email;
+    private Boolean isValidEmail;
+    private LocalDateTime createDt;
+    private LocalDateTime updateDt;
+    private LocalDateTime deleteDt;
 }
--- login_cookie/src/main/java/org/example/login_cookie/models/member/MemberEntity.java
+++ login_cookie/src/main/java/org/example/login_cookie/models/member/MemberEntity.java
@@ -1,4 +1,41 @@
-package org.example.login_cookie.models.member;
+package com.mjc813.login_cookie.models.member;
 
-public class MemberEntity {
+import jakarta.persistence.*;
+import lombok.*;
+import org.example.login_cookie.models.member.IMember;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity(name = "member")
+public class MemberEntity implements IMember {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "sing_id", nullable = false, length = 20, unique = true)
+    private String signId;
+
+    @Column(nullable = false, length = 200)
+    private String password;
+
+    @Column(nullable = false, length = 200, unique = true)
+    private String email;
+
+    @Column(name = "is_valid_email", nullable = false)
+    private Boolean isValidEmail;
+
+    @Column(name = "create_dt", nullable = false)
+    private LocalDateTime createDt;
+
+    @Column(name = "update_dt")
+    private LocalDateTime updateDt;
+
+    @Column(name = "delete_dt")
+    private LocalDateTime deleteDt;
 }
