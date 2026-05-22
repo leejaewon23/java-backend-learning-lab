60357ddcd8b63b92e273183ebfc24f786f5956d4
leejaewon23 <sosekee1110@naver.com>
2026-05-22 17:28:54 +0900

    feat: role 을 멤버에 추가했다. (GUEST, USER, ADMIN)

--- login_cookie/src/main/java/org/example/login_cookie/models/member/MemberEntity.java
+++ login_cookie/src/main/java/org/example/login_cookie/models/member/MemberEntity.java
@@ -18,7 +18,7 @@ public class MemberEntity implements IMember {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "sing_id", nullable = false, length = 20, unique = true)
+    @Column(name = "sign_id", nullable = false, length = 20, unique = true)
     private String signId;
 
     @Column(nullable = false, length = 200)
@@ -26,6 +26,9 @@ public class MemberEntity implements IMember {
 
     @Column(nullable = false, length = 200, unique = true)
     private String email;
+
+    @Column(nullable = false, length = 10)
+    private String role;
 
     @Column(name = "is_valid_email", nullable = false)
     private Boolean isValidEmail;
---
+++ login_cookie/src/main/java/org/example/login_cookie/models/role/Role.java
@@ -0,0 +1,7 @@
+package org.example.login_cookie.models.role;
+
+public enum Role {
+    GUEST,
+    USER,
+    ADMIN
+}
