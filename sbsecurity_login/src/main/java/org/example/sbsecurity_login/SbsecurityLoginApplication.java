d72307a368bf5f4763f753d058b32c3a25867345
leejaewon23 <sosekee1110@gmail.com>
2026-06-17 17:45:13 +0900

    init: spring security 프로젝트 생성

---
+++ sbsecurity_login/src/main/java/org/example/sbsecurity_login/SbsecurityLoginApplication.java
@@ -0,0 +1,13 @@
+package org.example.sbsecurity_login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbsecurityLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbsecurityLoginApplication.class, args);
+    }
+
+}
---
+++ sbsecurity_login/src/test/java/org/example/sbsecurity_login/SbsecurityLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sbsecurity_login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbsecurityLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
